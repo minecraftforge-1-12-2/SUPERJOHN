4739c0ec3043d9c23d9836208eb20027d5f359b2
forge 1.12.2 <150913393+minecraftforge-1-12-2@users.noreply.github.com>
2024-05-24 13:43:14 +0500

    optimize

--- src/main/java/net/minecraftforge/superjohn/block/ModBlocks.java
+++ src/main/java/net/minecraftforge/superjohn/block/ModBlocks.java
@@ -1,7 +1,6 @@
 package net.minecraftforge.superjohn.block;
 
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
